f1ca7c710cad70b7a1b8c28c293c20d7a2799264
Mohit Chawla <mcmohitc@gmail.com>
2021-06-22 16:02:55 +0530

    add json web token

--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
+const path = require('path');
+// require('dotenv/config');
+require('dotenv').config({ 
+    path: path.resolve(__dirname, `./config/${process.env.ENV || 'prod'}.env`)
+});
+
 module.exports = require('./server');
--- server/server.js
+++ server/server.js
@@ -6,11 +6,31 @@ const path = require('path');
 const { nanoid } = require('nanoid')
 const Client = require('./client');
 const Agent = require('./agent');
+const routes = require('./routes');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
 const clients = [];
 const agents = [];
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(function(req, res, next) {
+    res.sendData = function(obj) {
+      if (req.accepts('json') || req.accepts('text/html')) {
+        res.header('Content-Type', 'application/json');
+        res.send(obj);
+      } else {
+        res.send(406);
+      }
+    };
+  
+    next();
+  });
+
+app.use('/', routes);
 
 //initialize a simple http server
 const server = http.createServer(app);
@@ -36,8 +56,16 @@ wss.on('connection', (ws /*: WebSocket*/, req) => {
     
     if(query.auth){
         //validate auth
-        const agent = new Agent(nanoid(6), query.name, query.email, query.dept,  query.pid, ws);
-        agents.push(agent);
+        jwt.verify(query.auth, process.env.API_SECRET, (err, decoded) => {
+            if (err) {
+                ws.send(JSON.stringify({error: "Token is not valid"}));
+                ws.close();
+                return;
+            } else {
+                const agent = new Agent(nanoid(6), query.name, query.email, query.dept,  query.pid, ws);
+                agents.push(agent);
+            }
+        });        
     }else{
         
         const client = new Client(nanoid(6), query.name, query.email, query.dept,  query.pid, ws);
