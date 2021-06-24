ba635580cae38e14c76f66992079ffec737cc82e
rishabhsachdeva1234 <44360129+rishabhsachdeva1234@users.noreply.github.com>
2021-06-24 13:13:01 +0530

    agent-login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AgentDashboardComponent } from './agent-dashboard/agent-dashboard.component';
+import { AgentLoginComponent } from './agent-dashboard/agent-login/agent-login.component';
 import { SupportChatAdminComponent } from './support-chat-admin/support-chat-admin.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/agent' },
   { path: 'admin', component: SupportChatAdminComponent },
-  { path: 'agent', component: AgentDashboardComponent}
+  { path: 'agent', component: AgentDashboardComponent},
+  { path: 'agent-login', component: AgentLoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgZorroAntdModule} from './ng-zorro-antd.module';
 import { SupportChatAdminComponent } from './support-chat-admin/support-chat-admin.component';
 import { AgentDashboardComponent } from './agent-dashboard/agent-dashboard.component';
+import { AgentLoginComponent } from './agent-dashboard/agent-login/agent-login.component';
 
 registerLocaleData(en);
 
@@ -20,7 +21,8 @@ registerLocaleData(en);
   declarations: [
     AppComponent,
     SupportChatAdminComponent,
-    AgentDashboardComponent
+    AgentDashboardComponent,
+    AgentLoginComponent
   ],
   imports: [
     BrowserModule,
