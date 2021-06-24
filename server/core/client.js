976943c9cc3b8cd9231c40ef61c6c6ce281592c7
Naman Gupta <42265989+NamanGupta8@users.noreply.github.com>
2021-06-24 15:53:00 +0530

    agent name

--- server/core/client.js
+++ server/core/client.js
@@ -8,7 +8,7 @@ class Client{
         this.ws = ws;
 
         // this.ws.on('message', this.onMessage.bind(this))
-        if(agent) 
+        if(agent)
             this.ws.send(JSON.stringify({success: true, message: `Welcome ${this.name}`}));
         else
             this.ws.send(JSON.stringify({success: true, message: `Welcome ${this.name}. Please wait while an agent will be assigned to you.`}));
@@ -20,7 +20,7 @@ class Client{
     }
 
     assignAgent(agent){
-        this.ws.send(JSON.stringify({success: true, message: `Hi ${this.name}. Agent ${agent.name} is assigned to you.`}));
+        this.ws.send(JSON.stringify({success: true, message: `Hi ${this.name}. Agent ${agent.name} is assigned to you.`, name: agent.name}));
         this.agent = agent;
     }
 }
