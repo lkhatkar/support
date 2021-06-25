64e78e7ff1e9786ae7658714ec3a408d551640f6
Pranil <pranildani@gmail.com>
2021-06-25 14:02:52 +0530

    UI CHANGES

--- server/client/wssclient.js
+++ server/client/wssclient.js
@@ -1,21 +1,50 @@
-var client;
-function connect(name, email, dept = 'crane', pid = 'dummy') {
+var client, incoming_messages, outgoing_messages;
+
+if (localStorage.getItem('mail').value != null) {
+    connect(localStorage.getItem('name'), localStorage.getItem('mail'), localStorage.getItem('desig'));
+}
+
+
+function connect(name, email, dept, pid = "1") {
+    localStorage.setItem('mail', email);
+    localStorage.setItem('name', name);
     switchVisible();
     console.log('called', name, email);
-    client = new WssClient({ url: `ws:localhost?name=${name}&email=${email}&dept=${dept}&pid=${pid}` });
+    client = new WsClient({ url: `ws:localhost?name=${name}&email=${email}&dept=${dept}&pid=${pid}` });
     client.onMessage = function (msg) {
-        console.log(msg);
         var data = JSON.parse(msg.data);
-        if (data.message != '') {
-            document.getElementsByClassName('body')[0].innerHTML += `<div class="incoming">
+        if (data.name == undefined) {
+            console.log('undefined');
+            console.log(data);
+            if (data.message != '') {
+                incoming_messages += incoming_messages + "&&" + data.message;
+                localStorage.setItem('incoming', incoming_messages);
+                document.getElementsByClassName('body')[0].innerHTML += `<div class="incoming">
         <div class="bubble">
         <p>${data.message}</p>
         </div>`
+            }
+            document.getElementsByClassName('msg')[0].disabled = true;
+            document.getElementById('btn_sendMessage').disabled = true;
+            document.getElementById('btn_Attachment').disabled = true;
+        }
+        else {
+            if (data.message != '') {
+                incoming_messages += incoming_messages + "&&" + data.message;
+                localStorage.setItem('incoming', incoming_messages);
+                document.getElementsByClassName('body')[0].innerHTML += `<div class="incoming">
+        <div class="bubble">
+        <p>${data.message}</p>
+        </div>`
+            }
+            document.getElementsByClassName('msg')[0].disabled = false;
+            document.getElementById('btn_sendMessage').disabled = false;
+            document.getElementById('btn_Attachment').disabled = false;
         }
     }
 }
 
-class WssClient {
+class WsClient {
     constructor({ url }) {
         this.webSocket = new WebSocket(url);
         this.webSocket.onmessage = this._onMessage.bind(this);
@@ -28,7 +57,7 @@ class WssClient {
         }
     }
     send(message) {
-        this.webSocket.send(JSON.stringify(message));
+        this.webSocket.send(message);
     }
 }
 
@@ -50,6 +79,8 @@ function addmessage() {
     client.send(message);
     if (message != '') {
         console.log(message);
+        outgoing_messages += outgoing_messages + "&&" + message;
+        localStorage.setItem('outgoing', outgoing_messages);
         document.getElementsByClassName('body')[0].innerHTML += `<div class="outgoing">
         <div class="bubble">
         <p>${message}</p>
@@ -76,4 +107,23 @@ function switchVisible() {
             document.getElementsByClassName('foot')[0].style.display = 'flex';
         }
     }
+}
+
+
+function attach() {
+    var fileSelector = document.createElement('input');
+    fileSelector.setAttribute('id', 'attached_file');
+    fileSelector.setAttribute('type', 'file');
+    fileSelector.setAttribute('accept', '.jpg,.jpeg,.png')
+    fileSelector.click();
+
+    // var drawing = document.getElementsByClassName("msg")[0];
+    // var con = drawing.getContext("2d");
+    // var img = document.getElementById("attached_file");
+    // con.drawImage(img, 0, 0, 50, 50);
+    // var image2 = new Image();
+    // image2.src = "andyGoofy.gif";
+    // con.drawImage(image2, 100, 100, 70, 50);
+
+
 }
