005052815fd3da4d7d9e64425867653b3fc431a6
rishabhsachdeva1234 <44360129+rishabhsachdeva1234@users.noreply.github.com>
2021-06-28 10:50:57 +0530

    agent guard

--- src/app/guards/agent.guard.ts
+++ src/app/guards/agent.guard.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgentGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private router: Router, private service: AuthService) { }
+  canActivate() {
+    if(!this.service.agentLoggedIn()){
+      return true;
+    }
+    else {
+      this.router.navigate(['/agent-login']);
+      return false;
+    }
   }
-  
+
 }
