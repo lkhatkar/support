e194e92db7b699aa4d96067c5d73c27d480a668c
Pranil <pranildani@gmail.com>
2021-07-12 12:05:28 +0530

    Deleted Style.css & Script Generation
    Adding Script tag will now generate the whole html dynamically

--- server/client/wssclient.js
+++ server/client/wssclient.js
@@ -1,4 +1,326 @@
-var client, msg_image = false, msg_array = [], client_connected = false, saved_chat = '';
+var client, msg_array = [], client_connected = false, saved_chat = '';
+
+document.write(`<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .chatBtn {
+            bottom: 0;
+            position: fixed;
+            margin: 1em;
+            right: 0;
+            z-index: 998;
+            display: block;
+            width: 4em;
+            height: 4em;
+            border-radius: 50%;
+            border: none;
+            text-align: center;
+            color: #f0f0f0;
+            z-index: 998;
+            overflow: hidden;
+            background: #42a5f5;
+        }
+
+        .head {
+            margin: 0% 4%;
+            border-bottom: 2px solid #1a1a1b;
+        }
+
+        .chatPopup {
+            display: none;
+            width: 25%;
+            position: fixed;
+            bottom: 4.2em;
+            right: 4.2em;
+            border: 3px solid #f1f1f1;
+            z-index: 9;
+            height: 65%;
+        }
+
+        #closeChat {
+            display: none;
+        }
+
+        #openChat {
+            display: block;
+        }
+
+        body {
+            height: 100%;
+            font: 16px/1.2 "Roboto", sans-serif;
+            color: #333;
+        }
+
+        input[type="text"],
+        button {
+            border: 0;
+            outline: 0;
+        }
+
+        button {
+            background-color: transparent;
+            cursor: pointer;
+        }
+
+        button:hover i {
+            color: #79c7c5;
+            transform: scale(1.2);
+        }
+
+        /* chat_box */
+        .chat_box {
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+        }
+
+        .chat_box>* {
+            padding: 16px 0;
+        }
+
+        /* body */
+        .body {
+            display: none;
+            flex-grow: 1;
+            background-color: #eee;
+            overflow-y: scroll;
+            padding: 0% 3%;
+        }
+
+        .body .bubble {
+            display: inline-block;
+            padding: 0 3%;
+            border-radius: 25px;
+        }
+
+        .body .bubble p {
+            color: #f9fbff;
+            font-size: 14px;
+            text-align: left;
+        }
+
+        .body .incoming {
+            text-align: left;
+        }
+
+        .body .incoming .bubble {
+            background-color: #b2b2b2;
+            overflow-wrap: break-word;
+            max-width: 40%;
+        }
+
+        .body .outgoing {
+            text-align: right;
+        }
+
+        .body .outgoing .bubble {
+            background-color: #42a5f5;
+            overflow-wrap: break-word;
+            max-width: 40%;
+        }
+
+        #logout {
+            display: none;
+            float: right;
+            font-size: larger;
+        }
+
+        /* foot */
+        .foot {
+            display: none;
+            height: 4%;
+        }
+
+        .foot .msg {
+            flex-grow: 1;
+            background-color: white;
+        }
+
+        #btn_sendMessage {
+            font-size: large;
+            color: #1a1a1b;
+            padding: 0% 3%;
+        }
+
+        #btn_Attachment {
+            color: #1a1a1b;
+            font-size: large;
+            padding: 0 3%;
+        }
+
+        #name {
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            margin-top: 6px;
+            margin-bottom: 19px;
+            resize: vertical;
+        }
+
+        #mail {
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            margin-top: 6px;
+            margin-bottom: 19px;
+            resize: vertical;
+        }
+
+        #chatMsg {
+            border: none;
+        }
+
+        .info {
+            margin: 2% 4%;
+        }
+
+        input[type="Email"],
+        select,
+        textarea {
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            margin-top: 6px;
+            margin-bottom: 16px;
+            resize: vertical;
+        }
+
+        button[type="submit"] {
+            width: 60%;
+            display: inline-block;
+            font-weight: 400;
+            text-align: center;
+            white-space: nowrap;
+            vertical-align: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            border: 1px solid transparent;
+            padding: 0.375rem 0.75rem;
+            font-size: 1rem;
+            line-height: 1.5;
+            border-radius: 0.25rem;
+            transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+                border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+            color: #fff;
+            background-color: #545b62;
+            border-color: #4e555b;
+        }
+
+        button[type="submit"]:hover {
+            background-color: #1a1a1b;
+            color: white;
+        }
+
+        .time_date {
+            color: #747474;
+            display: block;
+            font-size: 12px;
+            margin: 3px 0 0;
+            padding-bottom: 5%;
+        }
+
+        img {
+            height: 80px;
+            width: 80px;
+            padding: auto;
+            margin: auto;
+        }
+
+        .submitSection {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <button class="chatBtn" onclick="openChat()" id="openChat"><i class="fa fa-comment"
+            style="font-size: 1.5em;"></i></button>
+    <button class="chatBtn" onclick="closeChat()" id="closeChat"><i class="fa fa-times"
+            style="font-size: 1.5em;"></i></button>
+
+    <div class="chatPopup" id="chatBox">
+        <div class="chat_box">
+
+            <div class="head">
+                <i class="fa fa-user" style="font-size: 1.5em;"></i>&nbsp Live Chat
+                <button style="float: right; font-size: larger; display: none;" id="logout" onclick="logout()"><i
+                        class="fa fa-sign-out"></i></button>
+            </div>
+
+            <div class="info">
+                <form name="login_form" onsubmit="return validateForm(event)">
+                    <label for="required">Name</label>
+                    <input type="text" name="Name" id="name">
+                    <label for="Email">Email</label>
+                    <input type="Email" name="Mail" id="mail">
+                    <label for="designation">Department</label>
+                    <select id="desig" name="desig">
+                        <option value="" selected disabled>Select Department</option>
+                        <option value="Sales">Sales</option>
+                        <option value="Billing">Billing</option>
+                        <option value="Support">Support</option>
+                    </select><br><br><br>
+                    <div class="submitSection">
+                        <button type="submit" value="submit">Start
+                            Chat</button>
+                    </div>
+
+                </form>
+            </div>
+
+            <div class="body">
+            </div>
+
+            <div class="foot">
+                <button id="btn_Attachment" onclick="attach()"><i class="fa fa-paperclip"></i></button>
+                <input type="file" id='attached_file' accept=".jpg,.jpeg,.png" hidden onchange='getImageData(event)'>
+                <input type="text" class="msg" placeholder="Type a message..." id='chatMsg' />
+                <button id="btn_sendMessage" onclick="addmessage()"><i class="fa fa-paper-plane"></i></button>
+            </div>
+
+
+        </div>
+    </div>
+
+    <script>
+        if (saved_chat != '') {
+            document.getElementsByClassName('info')[0].style.display = 'none';
+            document.getElementsByClassName('body')[0].style.display = 'block';
+            document.getElementsByClassName('foot')[0].style.display = 'flex';
+            document.getElementsByClassName('body')[0].innerHTML = '< br >' + saved_chat;
+            maill = localStorage.getItem('mail');
+            namee = localStorage.getItem('name');
+            deptt = localStorage.getItem('dept');
+            connect(namee, maill, deptt);
+        }
+        document.getElementsByClassName('msg')[0].addEventListener('keypress', function (e) {
+            if (e.keyCode == 13) {
+                addmessage();
+                return false;
+            }
+        })
+    </script>
+</body>
+
+</html>` );
+
+
 
 
 class WsClient {
@@ -101,10 +423,9 @@ function connect(name, email, dept, pid = "1") {
                     document.getElementById('btn_sendMessage').disabled = true;
                     document.getElementById('btn_Attachment').disabled = true;
                 }
-                else
-                {
+                else {
                     document.getElementsByClassName('body')[0].innerHTML += `<h5 style="text-align : center">
-                    No agents are available currently.</h3>`  
+                    No agents are available currently.</h3>`
                     document.getElementsByClassName('msg')[0].disabled = true;
                     document.getElementById('btn_sendMessage').disabled = true;
                     document.getElementById('btn_Attachment').disabled = true;
@@ -140,47 +461,26 @@ function connect(name, email, dept, pid = "1") {
 }
 
 function addmessage() {
-    if (msg_image) {
-        var file = document.getElementById('filename').files[0];
-        var reader = new FileReader();
-        var rawData = new ArrayBuffer();
-        reader.onload = function (e) {
-            rawData = e.target.result;
-            client.send(rawData);
-            alert("the File has been transferred.")
-        }
-        reader.readAsArrayBuffer(file);
+    var message = document.getElementsByClassName("msg")[0].value;
+    if (message != '') {
+        client.send(message);
         date = new Date();
-        var message = document.getElementsByClassName("msg")[0].value;
+        msg_obj = {
+            obj_message: message,
+            obj_isclient: true,
+            obj_date: date.toLocaleTimeString()
+        };
+        msg_array.push(msg_obj);
+        localStorage.setItem('messages', JSON.stringify(msg_array));
         document.getElementsByClassName('body')[0].innerHTML += `<div class="outgoing">
-        <div class="bubble">
-        <a>${message}</a>
-        </div>
-        <span class="time_date">${date.toLocaleTimeString()}</span></div>`
-        document.getElementsByClassName("msg")[0].value = '';
-    }
-    else {
-        var message = document.getElementsByClassName("msg")[0].value;
-        if (message != '') {
-            client.send(message);
-            date = new Date();
-            msg_obj = {
-                obj_message: message,
-                obj_isclient: true,
-                obj_date: date.toLocaleTimeString()
-            };
-            msg_array.push(msg_obj);
-            localStorage.setItem('messages', JSON.stringify(msg_array));
-            document.getElementsByClassName('body')[0].innerHTML += `<div class="outgoing">
             <div class="bubble">
             <p>${message}</p>
             </div>
             <span class="time_date">${date.toLocaleTimeString()}</span></div>`
-            document.getElementsByClassName("msg")[0].value = '';
-        }
-        else {
-            alert('type a message');
-        }
+        document.getElementsByClassName("msg")[0].value = '';
+    }
+    else {
+        alert('type a message');
     }
 }
 
@@ -201,17 +501,20 @@ function blobToDataURL(blob) {
     let reader = new FileReader();
     reader.onload = () => {
         let img = reader.result;
-        let imgTag = document.createElement('img');
-        imgTag.src = img;
-        document.getElementsByClassName('body')[0].appendChild(imgTag);
-        // var msgDiv = document.createElement('div');
-        // msgDiv.setAttribute('class','outgoing');
-        // msgDiv.cr
-        // document.getElementsByClassName('body')[0].innerHTML += `<div class="outgoing">
-        // <div class="bubble">
-        // ${document.appendChild(imgTag)}
-        // </div>
-        // <span class="time_date">${date.toLocaleTimeString()}</span>`
+        date = new Date();
+        msg_obj = {
+            obj_message: img,
+            obj_isclient: true,
+            obj_date: date.toLocaleTimeString()
+        };
+        msg_array.push(msg_obj);
+        localStorage.setItem('messages', JSON.stringify(msg_array));
+        document.getElementsByClassName('body')[0].innerHTML +=
+            `<div class="outgoing">
+        <div class="bubble">
+        <img src=${img}>
+        </div>
+        <span class="time_date">${date.toLocaleTimeString()}</span></div>`
     }
     reader.readAsDataURL(blob);
 }
