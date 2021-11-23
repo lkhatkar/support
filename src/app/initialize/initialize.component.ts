1929731ff11f313c61107aa8b8096a7953d6c572
Naman Gupta <42265989+NamanGupta8@users.noreply.github.com>
2021-11-23 11:25:59 +0530

    tabs setup for initial data

--- src/app/initialize/initialize.component.ts
+++ src/app/initialize/initialize.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators,FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-initialize',
@@ -8,24 +8,37 @@ import { FormBuilder, Validators,FormGroup } from '@angular/forms';
 })
 export class InitializeComponent implements OnInit {
   validateForm!: FormGroup;
+  currentIndex: number = 0;
+  tabs = [
+    {
+      name: 'Tab 1',
+      component: 'database-cred',
+      disabled: false
+    },
+    {
+      name: 'Tab 2',
+      component: 'admin-cred',
+      disabled: true
+    }
+  ];
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
-      postuser:['postgres' , [Validators.required]],
-      password:[null,[Validators.required]],
-      pghost:['localhost' ,[Validators.required]],
-      pgdatabase:[null, [Validators.required]],
-      pgport:['5432', [Validators.required]],
+      postuser: ['postgres', [Validators.required]],
+      password: [null, [Validators.required]],
+      pghost: ['localhost', [Validators.required]],
+      pgdatabase: [null, [Validators.required]],
+      pgport: ['5432', [Validators.required]],
     });
   }
-  submitForm():void {
-    
- for (const i in this.validateForm.controls) {
- this.validateForm.controls[i].markAsDirty();
- this.validateForm.controls[i].updateValueAndValidity();
- }
+  submitForm(): void {
+
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
 
 
   }
