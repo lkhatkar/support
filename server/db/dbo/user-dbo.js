ab27be965b88c382350091259ef0bc16261a1645
rishabhsachdeva1234 <rishabhsachdeva1234@gmail.com>
2021-12-06 15:40:51 +0530

    update user-dbo
    department_id added

--- server/db/dbo/user-dbo.js
+++ server/db/dbo/user-dbo.js
@@ -13,7 +13,7 @@ class UserDbo {
         PageId TEXT  NOT NULL,
         Create_Date  DATE  NOT NULL,
         IsActive  BOOLEAN NOT NULL DEFAULT (false),
-        Group_Id BIGINT
+        Department_Id BIGINT
                         REFERENCES Department (Sno) ON DELETE NO ACTION
                         ON UPDATE NO ACTION,
         Password TEXT,
@@ -54,18 +54,20 @@ class UserDbo {
       return this.dao.all(`SELECT * FROM userdata WHERE IsAgent = true`);
     }
 
-    create(Id, Name, Email, PageId, Create_Date, IsActive, Group_Id, Password, IsAgent) {
+    create(Id, Name, Email, PageId, Create_Date, IsActive, Department_Id, Password, IsAgent) {
         return this.dao.run(
-          'INSERT INTO userdata (Id, Name, Email, PageId, Create_Date, IsActive, Group_Id, Password, IsAgent) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *',
-          [Id, Name, Email, PageId, Create_Date, IsActive, Group_Id, Password, IsAgent])
+          'INSERT INTO userdata (Id, Name, Email, PageId, Create_Date, IsActive, Department_Id, Password, IsAgent) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *',
+          [Id, Name, Email, PageId, Create_Date, IsActive, Department_Id, Password, IsAgent])
     }
-    update(Sno, Id, Name, Email, PageId, Create_Date, IsActive, Group_Id, Password, IsAgent) {
-        // const {Sno, Id, Name, Email, PageId, Create_Date, IsActive, Group_Id, Password, IsAgent} = obj
+
+    update(Sno, Id, Name, Email, PageId, Create_Date, IsActive, Department_Id, Password, IsAgent) {
+        // const {Sno, Id, Name, Email, PageId, Create_Date, IsActive, Department_Id, Password, IsAgent} = obj
         return this.dao.run(
-            `UPDATE userdata SET Id = $1, Name = $2, Email = $3, PageId = $4, Create_Date = $5, IsActive = $6, Group_Id = $7, Password = $8, IsAgent = $9  WHERE Sno = $10`,
-            [Id, Name, Email, PageId, Create_Date, IsActive, Group_Id, Password, IsAgent, Sno]
+            `UPDATE userdata SET Id = $1, Name = $2, Email = $3, PageId = $4, Create_Date = $5, IsActive = $6, Department_Id = $7, Password = $8, IsAgent = $9  WHERE Sno = $10`,
+            [Id, Name, Email, PageId, Create_Date, IsActive, Department_Id, Password, IsAgent, Sno]
         )
     }
+
     delete(id) {
         return this.dao.run(
           `DELETE FROM userdata WHERE Sno = $1`,
